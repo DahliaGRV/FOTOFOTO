4a865c85fc53ee0eb0b3fcd77e331bad0e55c422
Lucas Roman <lucas.e.roman@gmail.com>
2022-05-09 22:21:04 -0700

    created api folder and put the api routes inside, created each handlbar for each rendered page minus html, created all routes to the pages and linked controllers. All routes are functioning in a basic unauthenticated way

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const {User} = require("../models/");
+const {User} = require("../../models");
 
 //find all
 router.get("/", (req, res) => {
