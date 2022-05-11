d8e4373a84f6d5b60b24635f3c7133ba32b32cae
Dahlia Graves <dahlialolagraves@gmail.com>
2022-05-11 11:04:44 -0700

    added in handlebars to hoempage and account, added a logout to account page.

--- controllers/homePageRoutes.js
+++ controllers/homePageRoutes.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 const express = require("express");
+const { User } = require("../models");
 
-router.get("/", (req, res) => {
-  if (!req.session.user) {
+router.get("/",async(req, res) => {
+ try{ if (!req.session.user) {
     return res.redirect("/");
   }
-  res.render("homepage");
+  const currentUser = await User.findByPk(
+    req.session.user.id)
+  const cleanData = currentUser.get({ plain: true });
+    console.log(cleanData);
+    res.render('homepage', cleanData);
+} catch (err){
+console.log(err);
+res.status(500).json({ msg: "an error occured", err });
+}
 });
 
 module.exports = router;
