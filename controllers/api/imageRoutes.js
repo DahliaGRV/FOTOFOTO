d46b645408ea40a055982cf70508ccf14cc03961
Joe Choe <joechoe125@gmail.com>
2022-05-10 21:55:39 -0700

    basic updates/formatting

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -1,27 +1,26 @@
-const { result } = require('lodash');
-const { Model, DataTypes } = require('sequelize');
-const router = require('..');
-const sequelize = require('../../config/connection');
+const express = require("express");
+const router = express.Router();
 
-class Image extends Model{}
+const { Image } = require("../../models");
+
 
 router.get("/images", (req, res) => {
     User.findAll({})
-      .then((dbImages) => {
-        console.log(dbImages);
-        res.json(dbImages);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
-      });
-  });
+        .then((dbImages) => {
+            console.log(dbImages);
+            res.json(dbImages);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ msg: "an error occured", err });
+        });
+});
 
-  router.post("/images",(req,res)=>{
-      Image.create({
-          where: {
-              filename:result.info.secure_url
-          }
-      })
-  });
-module.exports=Image
+router.post("/api/images", (req, res) => {
+    Image.create({
+        where: {
+            filename: result.info.secure_url
+        }
+    })
+});
+module.exports = Image
--- public/js/upload_widget.js
+++ public/js/upload_widget.js
@@ -8,101 +8,101 @@ console.log("linked");
 // can add see:
 //   https://cloudinary.com/documentation/upload_widget_reference
 
-const myWidget = cloudinary.createUploadWidget(
-  {
-    cloudName: cloudName,
-    uploadPreset: uploadPreset,
-    sources: [
-      "local",
-      "camera",
-      "google_drive",
-      "dropbox",
-  ],
-  googleApiKey: "<image_search_google_api_key>",
-  showAdvancedOptions: true,
-  cropping: true,
-  multiple: false,
-  defaultSource: "local",
-  styles: {
-      palette: {
-          window: "#5D005D",
-          sourceBg: "#3A0A3A",
-          windowBorder: "#AD5BA3",
-          tabIcon: "#ffffcc",
-          inactiveTabIcon: "#FFD1D1",
-          menuIcons: "#FFD1D1",
-          link: "#ffcc33",
-          action: "#ffcc33",
-          inProgress: "#00e6b3",
-          complete: "#a6ff6f",
-          error: "#ff1765",
-          textDark: "#3c0d68",
-          textLight: "#fcfffd"
-      },
-      fonts: {
-          default: null,
-          "'Kalam', cursive": {
-              url: "https://fonts.googleapis.com/css?family=Kalam",
-              active: true
-          }
-      }
-  }
-},
+const myWidget = cloudinary.createUploadWidget({
+            cloudName: cloudName,
+            uploadPreset: uploadPreset,
+            sources: [
+                "local",
+                "camera",
+                "google_drive",
+                "dropbox",
+            ],
+            googleApiKey: "<image_search_google_api_key>",
+            showAdvancedOptions: true,
+            cropping: true,
+            multiple: false,
+            defaultSource: "local",
+            styles: {
+                palette: {
+                    window: "#5D005D",
+                    sourceBg: "#3A0A3A",
+                    windowBorder: "#AD5BA3",
+                    tabIcon: "#ffffcc",
+                    inactiveTabIcon: "#FFD1D1",
+                    menuIcons: "#FFD1D1",
+                    link: "#ffcc33",
+                    action: "#ffcc33",
+                    inProgress: "#00e6b3",
+                    complete: "#a6ff6f",
+                    error: "#ff1765",
+                    textDark: "#3c0d68",
+                    textLight: "#fcfffd"
+                },
+                fonts: {
+                    default: null,
+                    "'Kalam', cursive": {
+                        url: "https://fonts.googleapis.com/css?family=Kalam",
+                        active: true
+                    }
+                }
+            }
+        },
 
-  (error, result) => {
-    if (!error && result && result.event === "success") {
-      console.log("Done! Here is the image info: ", result.info);
+        (error, result) => {
+            if (!error && result && result.event === "success") {
+                console.log("Done! Here is the image info: ", result.info);
 
-      // document
-      //   .getElementById("uploadedimage")
-      //   .setAttribute("src", result.info.secure_url);
-      console.log(result.info.secure_url, "this is the url");
-      // const newImage = result.info.secure_url
-    }
-  }
-).then((newImage)=>{
-  const urlHandler = async (event) => {
-    // event.preventDefault();
-    console.log(result.info.secure_url, "this url is visibile");
-    const response = await fetch("/images", {
-      method: "POST",
-      body: JSON.stringify(),
-      headers: { "Content-Type": "application/json" },
-    });
-    const newImage = result.info.secure_url
-  
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Image couldn't be uploaded");
-    }
-  };
+                // document
+                //   .getElementById("uploadedimage")
+                //   .setAttribute("src", result.info.secure_url);
+                console.log(result.info.secure_url, "this is the url");
+                // const newImage = result.info.secure_url
+            }
+        }
+    )
+    // .then((newImage)=>{
+    //   const urlHandler = async (event) => {
+    //     // event.preventDefault();
+    //     console.log(result.info.secure_url, "this url is visibile");
+    //     const response = await fetch("/images", {
+    //       method: "POST",
+    //       body: JSON.stringify(),
+    //       headers: { "Content-Type": "application/json" },
+    //     });
+    //     const newImage = result.info.secure_url
 
-})
-  
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert("Image couldn't be uploaded");
+//     }
+//   };
+
+// })
+
 
 
 
 document.getElementById("upload_widget").addEventListener(
-  "click",
-  function () {
-    myWidget.open();
-    console.log("linked!");
-  },
-  false
+    "click",
+    function() {
+        myWidget.open();
+        console.log("linked!");
+    },
+    false
 );
-// const urlHandler = async (event) => {
-//   // event.preventDefault();
-//   console.log(result.info.secure_url, "this url is visibile");
-//   const response = await fetch("/images", {
-//     method: "POST",
-//     body: JSON.stringify(),
-//     headers: { "Content-Type": "application/json" },
-//   });
+const urlHandler = async(event) => {
+    // event.preventDefault();
+    console.log(result.info.secure_url, "this url is visible");
+    const response = await fetch("/api/images", {
+        method: "POST",
+        body: JSON.stringify(),
+        headers: { "Content-Type": "application/json" },
+    });
 
-//   if (response.ok) {
-//     document.location.replace("/");
-//   } else {
-//     alert("Image couldn't be uploaded");
-//   }
-// };
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        alert("Image couldn't be uploaded");
+    }
+};
