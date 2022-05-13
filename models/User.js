6c0d6d6322d6987067c08db5e72b9387952dd966
Joe Choe <joechoe125@gmail.com>
2022-05-13 13:45:26 -0700

    fixed signup redirect. Created alert notification

--- models/User.js
+++ models/User.js
@@ -4,65 +4,62 @@ const bcrypt = require("bcrypt");
 
 class User extends Model {}
 
-User.init(
-  {
-    // add properites here, ex:
+User.init({
+    // add properties here, ex:
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
     },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4],
+        },
     },
     avatar: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     background: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     theme: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     font: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
-  },
-  {
+}, {
     hooks: {
-      beforeCreate: async (userdata) => {
-        userdata.password = await bcrypt.hash(userdata.password, 5);
-        return userdata;
-      },
+        beforeCreate: async(userdata) => {
+            userdata.password = await bcrypt.hash(userdata.password, 5);
+            return userdata;
+        },
     },
     sequelize,
     timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: "user",
-  }
-);
+});
 
 module.exports = User;
