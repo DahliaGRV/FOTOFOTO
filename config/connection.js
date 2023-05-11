9fd16d6e7ffa0d429b8a4a3f5850b6b39a5ae0a3
Dahlia Graves <dahlialolagraves@gmail.com>
2023-05-10 20:02:59 -0700

    added some stuff my mysql2

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const mysql2 = require('mysql2');
 
 let sequelize;
 
@@ -7,6 +8,7 @@ let sequelize;
 // if (process.env.JAWSDB_URL) {
 //     sequelize = new Sequelize(process.env.JAWSDB_URL);
 // } else {
+
 sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -14,7 +16,8 @@ sequelize = new Sequelize(
     {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3306,
+        dialectModule: mysql2,
     }
 );
 // }
